beac44f551c246483f3e6524401d445671173b27
henhengirl <xiaoshanshan_java@163.com>
2019-04-03 10:57:19 +0800

    new project

---
+++ src/main/java/com/enjoy/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.enjoy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/git")
+public class TestController {
+
+    @RequestMapping(value = "/test")
+    public Object test(){
+        return "Hello , git!";
+    }
+}
